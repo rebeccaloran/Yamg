445ecbf696fb5bfa2b2f73e01c37a58756699de1
areckx <alex.loranger.1988@gmail.com>
2014-06-01 21:36:01 +0000

    AHHH

--- js/html5games.memory.js
+++ js/html5games.memory.js
@@ -3,6 +3,7 @@ $(function() {
 
         // Create global objects
         var memory = {}
+<<<<<<< HEAD
 
 
         // Create array of cards
@@ -42,6 +43,34 @@ $(function() {
 
 
 
+=======
+        grid = document.getElementById('grid')
+
+        memory.srcs = [ 
+          "../img/card_chocolate.png:
+          "../img/card_coffee.png:
+          "../img/card_leopard.png:
+          "../img/card_lightning.png:
+          "../img/card_space.png:
+          "../img/card_warning.png:
+        ];
+
+        memory.li = [];
+        memory.img = [];
+        memory.frag = []; 
+
+
+        memory.frag = document.createDocumentFragment()
+        memory.srcs.forEach(function(src) {
+          memory.li = document.createElement('li')
+          memory.img = new Image()
+          memory.img.src = src
+          memory.img..className = 'card'
+          memory.li.appendChild(img)
+          memory.frag.appendChild(li)
+        })
+        grid.appendChild(frag)
+>>>>>>> parent of 78a81af... gonna revert
 });
 
 

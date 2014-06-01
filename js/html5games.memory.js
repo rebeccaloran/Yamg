78a81af8ef98effba66425df44efe5bbc8e1b2b7
areckx <alex.loranger.1988@gmail.com>
2014-06-01 21:17:16 +0000

    gonna revert

--- js/html5games.memory.js
+++ js/html5games.memory.js
@@ -3,30 +3,31 @@ $(function() {
 
 
         var memory = {}
-        grid = document.getElementById('grid')
+       memory.grid = document.getElementById('grid')
 
         memory.srcs = [ 
-          "../img/card_chocolate.png:
-          "../img/card_coffee.png:
-          "../img/card_leopard.png:
-          "../img/card_lightning.png:
-          "../img/card_space.png:
-          "../img/card_warning.png:
-        ];
+          "img/card_chocolate.png",
+          "img/card_coffee.png",
+          "img/card_leopard.png",
+          "img/card_lightning.png",
+          "img/card_space.png",
+          "img/card_warning.png"
+        ]
 
         memory.li = [];
         memory.img = [];
         memory.frag = []; 
 
 
-        memory.frag = document.createDocumentFragment()
+        memory.frag = document.createDocumentFragment();
         memory.srcs.forEach(function(src) {
-          memory.li = document.createElement('li')
-          memory.img = new Image()
-          memory.img.src = src
-          memory.img..className = 'card'
-          memory.li.appendChild(img)
-          memory.frag.appendChild(li)
-        })
-        grid.appendChild(frag)
+        memory.btn = document.createElement('button');
+        memory.img = new Image();
+        memory.img.src = src;
+        memory.img.className = 'card';
+        memory.button.appendChild(memory.img);
+        memory.frag.appendChild(memory.button);
+        
+        });
+        memory.grid.appendChild(memory.frag);
 });

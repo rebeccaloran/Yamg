9157940680526cacbad1efabd0e45bf3bebce272
areckx <alex.loranger.1988@gmail.com>
2014-06-01 21:18:34 +0000

    going to revert

--- js/html5games.memory.js
+++ js/html5games.memory.js
@@ -1,9 +1,15 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 $(function() {
+=======
+$(function() { 
+>>>>>>> parent of 7ed64cc... started new track
 
 
+        // Create global objects
         var memory = {}
+<<<<<<< HEAD
        memory.grid = document.getElementById('grid')
 
         memory.srcs = [ 
@@ -43,9 +49,51 @@ $(function() {
         
                 for(var j = 0; j < rowCount; j++) {
                         document.body.innerHTML += '<a href="#" id="card_back">[]</a>';
+=======
+
+
+        // Create array of cards
+        memory.cards = ["#card_chocolate", "#card_coffee", "#card_leopard"
+        , "#card_lightning", "#card_space", "#card_warning"] 
+
+        // create a 3x10 grid for the cards 
+        memory.rowCount = 3;
+        memory.columnCount = 10;
+/*
+        // I find sorting them randomly then showing the first 4 to be the easiest, like this:
+        memory.divs = $("div.Image").get().sort(function(){ 
+                    return Math.round(Math.random())-0.5; //so we get the right +/- combo
+                   }).slice(0,4);
+        $(divs).show();
+ 
+
+        //You can test it out here. If you want to also randomize the order (not just which are shown), 
+        //you already have them sorted so just append them to the same parent in their new order by changing this: 
+        $(divs).show();
+        //to this:
+        $(divs).appendTo(divs[0].parentNode).show();
+*/
+
+
+
+        for(var i = 0; i < memory.rowCount; i++) {
+               document.body.innerHTML += '<div class="row">'; 
+        
+                for(var j = 0; j < memory.columnCount; j++) {
+                        document.body.innerHTML += '<button  href="#row' 
+                        + (i) + '_' + (j) +  '" id="card_back"></button>';
+>>>>>>> parent of 7ed64cc... started new track
                 }
                
                 document.body.innerHTML += '</div>'; 
         }
+<<<<<<< HEAD
 >>>>>>> parent of 1141b68... got back of the cards, need to start working on flipping them. going to try loading images WITHIN js rather than css
+=======
+
+
+
+>>>>>>> parent of 7ed64cc... started new track
 });
+
+

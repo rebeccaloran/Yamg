1141b68c083bd9cd8b74907829c5129a5174f9c4
areckx <alex.loranger.1988@gmail.com>
2014-06-01 20:12:09 +0000

    got back of the cards, need to start working on flipping them. going to try loading images WITHIN js rather than css

--- js/html5games.memory.js
+++ js/html5games.memory.js
@@ -1,18 +1,47 @@
 $(function() { 
 
-        // create a 5x5 grid for the cards
 
-        var rowCount = 5;
+        // Create global objects
+        var memory = {}
 
-        for(var i = 0; i < rowCount; i++) {
+
+        // Create array of cards
+        memory.cards = ["#card_chocolate", "#card_coffee", "#card_leopard"
+        , "#card_lightning", "#card_space", "#card_warning"] 
+
+        // create a 3x10 grid for the cards 
+        memory.rowCount = 3;
+        memory.columnCount = 10;
+/*
+        // I find sorting them randomly then showing the first 4 to be the easiest, like this:
+        memory.divs = $("div.Image").get().sort(function(){ 
+                    return Math.round(Math.random())-0.5; //so we get the right +/- combo
+                   }).slice(0,4);
+        $(divs).show();
+ 
+
+        //You can test it out here. If you want to also randomize the order (not just which are shown), 
+        //you already have them sorted so just append them to the same parent in their new order by changing this: 
+        $(divs).show();
+        //to this:
+        $(divs).appendTo(divs[0].parentNode).show();
+*/
+
+
+
+        for(var i = 0; i < memory.rowCount; i++) {
                document.body.innerHTML += '<div class="row">'; 
         
-                for(var j = 0; j < rowCount; j++) {
-                        document.body.innerHTML += '<a href="#" id="card_back">[]</a>';
+                for(var j = 0; j < memory.columnCount; j++) {
+                        document.body.innerHTML += '<button  href="#row' 
+                        + (i) + '_' + (j) +  '" id="card_back"></button>';
                 }
                
                 document.body.innerHTML += '</div>'; 
         }
+
+
+
 });
 
 

7a79ec1af30f3422d0232f532198d7e6574917d1
areckx <alex.loranger.1988@gmail.com>
2014-06-02 01:33:17 +0000

    can now click on cards and produce random image; must figure out how to make a deck and map them to each classname (class=0; class=1 etc)

--- js/html5games.memory.js
+++ js/html5games.memory.js
@@ -1,32 +1,65 @@
+$(function() { 
 
-$(function() {
 
-
+        // Create global objects
         var memory = {}
-        grid = document.getElementById('grid')
-
-        memory.srcs = [ 
-          "../img/card_chocolate.png:
-          "../img/card_coffee.png:
-          "../img/card_leopard.png:
-          "../img/card_lightning.png:
-          "../img/card_space.png:
-          "../img/card_warning.png:
-        ];
-
-        memory.li = [];
-        memory.img = [];
-        memory.frag = []; 
 
 
-        memory.frag = document.createDocumentFragment()
-        memory.srcs.forEach(function(src) {
-          memory.li = document.createElement('li')
-          memory.img = new Image()
-          memory.img.src = src
-          memory.img..className = 'card'
-          memory.li.appendChild(img)
-          memory.frag.appendChild(li)
-        })
-        grid.appendChild(frag)
+        // Create array of cards
+        memory.cards = ["card_chocolate", "card_coffee", "card_leopard"
+        , "card_lightning", "card_space", "card_warning"] ;
+
+        // create a 3x10 grid for the cards 
+        memory.rowCount = 3;
+        memory.columnCount = 10;
+
+        setInterval(function() {
+        memory.random = memory.cards[Math.floor(Math.random()*memory.cards.length)];
+        });
+
+        memory.deck = [];
+        // Loop to create deck
+        for(var i = 0; i < 25; i++) {
+                memory.deck.push(memory.random); 
+        }
+        
+/*
+        // I find sorting them randomly then showing the first 4 to be the easiest, like this:
+        memory.divs = $("div.Image").get().sort(function(){ 
+                    return Math.round(Math.random())-0.5; //so we get the right +/- combo
+                   }).slice(0,4);
+        $(divs).show();
+ 
+
+        //You can test it out here. If you want to also randomize the order (not just which are shown), 
+        //you already have them sorted so just append them to the same parent in their new order by changing this: 
+        $(divs).show();
+        //to this:
+        $(divs).appendTo(divs[0].parentNode).show();
+*/
+
+
+        // Create the grid of all face down cards 
+        for(var i = 0; i < memory.rowCount; i++) {
+               document.body.innerHTML += '<div class="row">'; 
+        
+                for(var j = 0; j < memory.columnCount; j++) {
+                        document.body.innerHTML += '<img  class="' 
+                        + (25) +  '" id="card_back"></img>';
+                }
+               
+                document.body.innerHTML += '</div>'; 
+        } 
+
+        // When clicking any of the elements, return className of clicked element
+        $("img").click(function() {
+                //alert(this.className);
+                // create a css rule 
+                $(this).replaceWith('<img class="25" '
+                        + 'id="' + memory.random + '"></img>');
+
+               
+        });
 });
+
+

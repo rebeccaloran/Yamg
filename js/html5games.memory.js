cb89415540eed0b39c8f01e204c908c85663fcc0
areckx <alex.loranger.1988@gmail.com>
2014-06-02 02:21:00 +0000

    zzzzzzz

--- js/html5games.memory.js
+++ js/html5games.memory.js
@@ -12,6 +12,8 @@ $(function() {
         // create a 3x10 grid for the cards 
         memory.rowCount = 3;
         memory.columnCount = 10;
+        // count the index for class name
+        memory.slice = 0;
 
         setInterval(function() {
         memory.random = memory.cards[Math.floor(Math.random()*memory.cards.length)];
@@ -19,7 +21,7 @@ $(function() {
 
         memory.deck = [];
         // Loop to create deck
-        for(var i = 0; i < 25; i++) {
+        for(var i = 0; i < 30; i++) {
                 memory.deck.push(memory.random); 
         }
         
@@ -40,12 +42,14 @@ $(function() {
 
 
         // Create the grid of all face down cards 
-        for(var i = 0; i < memory.rowCount; i++) {
+        for(var i = 0 ; i < memory.rowCount; i++) {
+               
                document.body.innerHTML += '<div class="row">'; 
         
                 for(var j = 0; j < memory.columnCount; j++) {
                         document.body.innerHTML += '<img  class="' 
-                        + (25) +  '" id="card_back"></img>';
+                        + (memory.slice) +  '" id="card_back"></img>';
+                       ++ memory.slice;
                 }
                
                 document.body.innerHTML += '</div>'; 
@@ -55,8 +59,8 @@ $(function() {
         $("img").click(function() {
                 //alert(this.className);
                 // create a css rule 
-                $(this).replaceWith('<img class="25" '
-                        + 'id="' + memory.random + '"></img>');
+                $(this).replaceWith('<img class="' + memory.slice
+                        + '" id="' + memory.random + '"></img>');
 
                
         });
